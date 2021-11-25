dcc97cc3ab6b89dc3846d6f29ae987f3f237a883
durumi99 <kms990615@naver.com>
2021-11-25 11:43:09 +0900

    ,

--- SpartanProject/App.js
+++ SpartanProject/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import MainComponent from './components/MainContainer';
 import RestaurantList from './components/RestaurantList';
 import AboutRestaurant from './components/AboutRestaurant';
+import Login from './components/Login';
+import Register from './components/Register';
 
 const IScrolledDownAndWhatHappenedNextShockedMe = () => {
   //return <RestaurantList />;
-  return <MainComponent />;
+  //return <MainComponent />;
   //return <AboutRestaurantt />;
+  //return <Login />;
+  return <Register />;
 };
 
 export default IScrolledDownAndWhatHappenedNextShockedMe;
